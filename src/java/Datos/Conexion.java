35eaf530cb606acc28faae3b4e8582315956a11b
57315 <57315@DESKTOP-SR697HE>
2020-08-02 16:57:03 -0500

    Se agregan nuevas clases_1

---
+++ src/java/Datos/Conexion.java
@@ -0,0 +1,25 @@
+package Datos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Conexion {
+
+    public Connection Conexion(){
+        Connection conexion=null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conexion = (Connection) DriverManager.  getConnection("jdbc:mysql://localhost/pf_agendarcitas","yune","yuneider");
+            System.out.println("completado");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return conexion;
+    }
+    
+    public static void main(String[] args) {
+        Conexion cbd = new Conexion();
+        cbd.Conexion();
+    }
+    
+}
