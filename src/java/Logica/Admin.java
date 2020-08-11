4defbf4057cf945c46968b6dd5d520fdcbbb938c
57315 <57315@DESKTOP-SR697HE>
2020-08-11 09:24:54 -0500

    Creación de la clase admin y la funcion de enviar correos

---
+++ src/java/Logica/Admin.java
@@ -0,0 +1,51 @@
+package Logica;
+
+import com.sun.speech.freetts.Voice;
+import com.sun.speech.freetts.VoiceManager;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class Admin {
+
+    public static void main(String[] args) {
+        EnviarCorreo("jhonycaro1998@gmail.com","holi","prueba de mariconeria salió positiva");
+    }
+    
+    public static void EnviarCorreo(String correo,String mensaje, String asunto){
+        Properties propiedad = new Properties();
+        propiedad.setProperty("mail.smtp.host", "smtp.gmail.com");
+        propiedad.setProperty("mail.smtp.starttls.enable", "true");
+        propiedad.setProperty("mail.smtp.port", "587");
+        propiedad.setProperty("mail.smtp.auth", "true");
+        Session sesion = Session.getDefaultInstance(propiedad);
+        String correoEnvia = "cgpmedicineplus@gmail.com";
+        String contrasena = "CGPMedicine";
+        MimeMessage mail = new MimeMessage(sesion);
+        try {
+            mail.setFrom(new InternetAddress(correoEnvia));
+            mail.addRecipient(Message.RecipientType.TO, new InternetAddress(correo));
+            mail.setSubject(asunto);
+            mail.setText(mensaje);
+            Transport transporte = sesion.getTransport("smtp");
+            transporte.connect(correoEnvia,contrasena);
+            transporte.sendMessage(mail, mail.getRecipients(Message.RecipientType.TO));
+            transporte.close();
+        } catch (MessagingException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    public static void ApaHabla(String mensaje){
+        VoiceManager manager = VoiceManager.getInstance();
+        Voice voz = manager.getVoice("kevin16");
+        voz.allocate();
+        voz.speak(mensaje);
+        voz.deallocate();
+    }
+    
+}
